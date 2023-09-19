8276bb5cf1d572f81ab46f7a96280950126aa2ad
MaksimUlitin <maksUlitin59@gmail.com>
2023-09-19 20:19:38 +0300

    intermediate result

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Block struct{}
 
@@ -11,9 +13,30 @@ func main() {
 }
 
 func run() error {
-
+	return nil
 }
 
 func makeMuxRouter() http.Handler {
+	HandlerFunc("/", handleGetBlockchain).Method("GET")
+	HandlerFunc("/", handleWriteBlock).Method("POST")
+}
 
+func handleGetBlockchain() {}
+
+func handleWriteBlock() {}
+
+func respondWithJSON() {}
+
+func isBlockValid() bool {
+	return true
+}
+
+func calculateHash() string {
+	return ""
+}
+
+func generateBlock() {}
+
+func isHashValid() bool {
+	return true
 }
